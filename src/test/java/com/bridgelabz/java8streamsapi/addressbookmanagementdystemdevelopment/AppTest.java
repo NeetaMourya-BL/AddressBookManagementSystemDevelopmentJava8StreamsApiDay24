c898e478142a135d51c2c09cb00e01c32319dbdd
unknown <neetamourya24@gmail.com>
2021-12-20 14:15:31 +0530

    [Neeta mourya] create contact in UC1 branch

---
+++ src/test/java/com/bridgelabz/java8streamsapi/addressbookmanagementdystemdevelopment/AppTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.java8streamsapi.addressbookmanagementdystemdevelopment;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
